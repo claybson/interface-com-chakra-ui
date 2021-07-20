cfaa4d0e0050b3cf3bcc73a19da319b79245b254
Claybson <claybson@cartello.com.br>
2021-07-20 16:59:10 -0300

    finalizando desafio

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,35 +1,39 @@
-import { Flex, Grid, Image } from "@chakra-ui/react";
+import { Flex, Grid, GridItem, Icon, Image } from "@chakra-ui/react";
+import { useRouter } from "next/dist/client/router";
+import { RiArrowLeftSLine } from 'react-icons/ri';
+import Link from 'next/link';
 
 export default function Header() {
-    return (
-        <Flex 
-            bg="white" 
-            w="100%" 
-            as="header" 
-            mx="auto" 
-            px="1rem" 
-            h={["50px", "100px"]} 
-            align="center" 
-            justify="center"
-        >
-            <Grid
-                h="100%"
-                mx="auto"
-                w="100%"
-                maxW="1160px"
-                alignItems="center"
-                templateColumn="repeat(3, 1fr)"
-                justifyContent="center"
-            >
-                <Image 
-                    src="/logo.svg"
-                    alt="Um avião voando sobre o nome da marca"
-                    justifySelf="center"
-                    gridColumn="2"
-                    w={["81px", "184px"]}
-                />
+  const { asPath } = useRouter()
+  const notHomePage = asPath !== '/'
 
-            </Grid>
-        </Flex>
-    )
+  return (
+    <Flex bg="white" w="100%" as="header" mx="auto" px="1rem" h={["50px","100px"]} align="center" justify="center">
+      <Grid
+        h="100%"
+        mx="auto"
+        w="100%"
+        maxW="1160px"
+        alignItems="center"
+        templateColumns="repeat(3, 1fr)"
+        justifyContent="center"
+        alignSelf="start"
+      >
+        {notHomePage && (
+          <Link href="/">
+            <a>
+              <Icon as={RiArrowLeftSLine} fontSize={[20,40]} justifySelf="start"/>
+            </a>
+          </Link>
+        )}
+          <Image
+            w={["81px","184px"]}
+            src="/logo.svg"
+            alt="Um avição voando sobre o nome da marca world trip"
+            justifySelf="center"
+            gridColumn="2"
+          />
+      </Grid>
+    </Flex>
+  )
 }
